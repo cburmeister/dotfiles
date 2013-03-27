6125a8991859a440c69775d99d60b37b2739e642
Corey Burmeister <burmeister.corey@gmail.com>
2013-03-26 23:02:32 -0700

    update submodules

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ echo "Deleting the old files..."
 rm ~/.vimrc
 rm ~/.vim
 rm ~/.ackrc
+rm ~/.zshrc
 
 echo "Symlinking files..."
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/ackrc ~/.ackrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 echo "Updating submodules..."
 git submodule foreach git pull origin master --recurse-submodules
