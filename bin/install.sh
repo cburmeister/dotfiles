1e111df41fddeb2080dacbe655171b3c810104f9
Corey Burmeister <burmeister.corey@gmail.com>
2013-04-29 14:11:26 -0700

    update install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-echo "Initializing submodules..."
+echo "[+] Initializing submodules..."
 git submodule init
 git submodule update
 
-echo "Deleting the old files..."
+echo "[+] Deleting the old files..."
 rm ~/.vimrc
 rm ~/.vim
 rm ~/.ackrc
 rm ~/.zshrc
 rm ~/.tmux.conf
 rm ~/.gitconfig
+rm ~/.pythonrc.py
+rm ~/.editrc
 
-echo "Symlinking files..."
+echo "[+] Symlinking files..."
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/ackrc ~/.ackrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/pythonrc.py ~/.pythonrc.py
+ln -s ~/.dotfiles/editrc ~/.editrc
 
-echo "Updating submodules..."
+echo "[+] Updating submodules..."
 git submodule foreach git pull origin master --recurse-submodules
 
-echo "All done..."
+echo "[+] Done..."
