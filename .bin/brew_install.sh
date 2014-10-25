5baf148312e82944a351ab57432db521bd767fa7
Corey Burmeister <burmeister.corey@gmail.com>
2014-10-25 16:32:31 -0700

    Replaces Brewfile with brew_install.sh
    Brew bundle is no longer supported:
    
    https://github.com/Homebrew/homebrew/issues/30815

---
+++ .bin/brew_install.sh
@@ -0,0 +1,41 @@
+brew update
+
+brew tap homebrew/versions
+brew tap phinze/homebrew-cask
+
+brew install ack
+brew install autoenv
+brew install brew-cask
+brew install elasticsearch
+brew install exiftool
+brew install gcc
+brew install git
+brew install httpie
+brew install libjpeg
+brew install memcached
+brew install mongodb
+brew install node
+brew install pass
+brew install pianobar
+brew install pyenv
+brew install pyenv-virtualenvwrapper
+brew install rbenv
+brew install rbenv-gem-rehash
+brew install redis
+brew install rtorrent
+brew install ruby-build
+brew install terminal-notifier
+brew install tig
+brew install tmux
+brew install wget
+brew install zsh
+
+brew cask install audacity
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install slack
+brew cask install tunnelblick
+brew cask install virtualbox
+brew cask install vagrant
+
+brew cleanup
