c6e48f7a5fc0f85bb7415f718cb4a0eec98bba94
Corey Burmeister <burmeister.corey@gmail.com>
2015-02-24 09:34:46 -0800

    Remove pianobar notify stuff
    This functionality is replaced by:
    
    https://github.com/cburmeister/pianobar-notify

--- .config/pianobar/notify.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-trigger = ARGV.shift
-
-if trigger == 'songstart'
-    songinfo = {}
-    STDIN.each_line { |line| songinfo.store(*line.chomp.split('=', 2))}
-    `terminal-notifier -message "Artist: #{songinfo['artist']}\nAlbum: #{songinfo['album']}" -title "#{songinfo['title']}"`
-end
